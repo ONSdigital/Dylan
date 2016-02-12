e23bd21076039375d772d807c3e8b2b270f78284
David Carboni <david@carboni.io>
2016-02-12 11:57:00 +0000

    Now uses the CryptoFSProvider properly.

--- src/main/java/com/github/davidcarboni/dylan/filesystem/CryptoFSFactory.java
+++ src/main/java/com/github/davidcarboni/dylan/filesystem/CryptoFSFactory.java
@@ -1,9 +1,11 @@
 package com.github.davidcarboni.dylan.filesystem;
 
+import com.sun.jndi.toolkit.url.Uri;
 import org.apache.sshd.common.file.FileSystemFactory;
 import org.apache.sshd.common.session.Session;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.FileSystem;
 
 /**
@@ -12,6 +14,8 @@ import java.nio.file.FileSystem;
 public class CryptoFSFactory implements FileSystemFactory {
     @Override
     public FileSystem createFileSystem(Session session) throws IOException {
-        return null; // TODO CryptoFS.getInstance();
+        CryptoFSProvider cryptoFSProvider = CryptoFSProvider.getInstance();
+        URI uri = URI.create(cryptoFSProvider.getScheme()+"///");
+        return cryptoFSProvider.getFileSystem(uri);
     }
 }
